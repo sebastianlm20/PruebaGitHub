cfe42332fc2256a4e1607fbdb577a085746beebc
Sebastian <sebasti.m.0310@gmail.com>
2026-02-20 18:04:11 -0500

    cambio para probar actualizacion

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
     $nombre = "Sebastian";
     echo "<h1>Hola, $nombre, este es el codigo inicial del repositorio</h1>";
     echo "otro change";
-    echo "Esto es un cambio hecho por otro integrante que no es el lider"
+    echo "Esto es un cambio hecho por otro integrante que no es el lider";
+
+    echo "Este cambio es para ver como se debe actualizar";
